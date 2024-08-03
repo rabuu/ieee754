4442ffb2c68ff5b07fa7c3a74b633c488d8eae6f
Rasmus Buurman <dev@rbuurman.de>
2024-08-03 15:48:14 +0200

    Better CLI interface but nasty FloatWrapper

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::path::PathBuf;
+use std::{fmt::Display, path::PathBuf};
 
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 use plotters::prelude::*;
 
-use ieee754::{Ieee754, SmallFloat, Value};
+use ieee754::{DoubleFloat, QuadrupleFloat, SingleFloat, SmallFloat, Value};
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -13,21 +13,98 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Cmd {
-    Plot {
-        output_file: PathBuf,
-    },
-
     #[command(alias = "eval")]
     Evaluate {
+        /// The float number as a sequence of 0s and 1s
         binary: String,
+
+        /// The float format
+        #[arg(short, long, default_value = "single")]
+        format: FloatFormat,
 
         #[arg(short, long)]
         raw: bool,
     },
 
-    Test {
-        binary: String,
+    /// Plot the distribution of all SmallFloat values
+    Plot {
+        /// Output file where to write the plotted image
+        output_file: PathBuf,
     },
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum FloatFormat {
+    Small,
+    Single,
+    Double,
+    Quadruple,
+}
+
+#[derive(Debug)]
+enum FloatWrapper {
+    Small(SmallFloat),
+    Single(SingleFloat),
+    Double(DoubleFloat),
+    Quadruple(QuadrupleFloat),
+}
+
+impl FloatWrapper {
+    fn parse(binary: &str, format: FloatFormat) -> Result<FloatWrapper, String> {
+        Ok(match format {
+            FloatFormat::Small => FloatWrapper::Small(SmallFloat::parse(binary)?),
+            FloatFormat::Single => FloatWrapper::Single(SingleFloat::parse(binary)?),
+            FloatFormat::Double => FloatWrapper::Double(DoubleFloat::parse(binary)?),
+            FloatFormat::Quadruple => FloatWrapper::Quadruple(QuadrupleFloat::parse(binary)?),
+        })
+    }
+
+    fn raw_value(&self) -> f32 {
+        match self {
+            FloatWrapper::Small(x) => x.raw_value(),
+            FloatWrapper::Single(x) => x.raw_value(),
+            FloatWrapper::Double(x) => x.raw_value(),
+            FloatWrapper::Quadruple(x) => x.raw_value(),
+        }
+    }
+
+    fn evaluate(&self) -> Value {
+        match self {
+            FloatWrapper::Small(x) => x.evaluate(),
+            FloatWrapper::Single(x) => x.evaluate(),
+            FloatWrapper::Double(x) => x.evaluate(),
+            FloatWrapper::Quadruple(x) => x.evaluate(),
+        }
+    }
+}
+
+impl Display for FloatWrapper {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FloatWrapper::Small(x) => x.fmt(f),
+            FloatWrapper::Single(x) => x.fmt(f),
+            FloatWrapper::Double(x) => x.fmt(f),
+            FloatWrapper::Quadruple(x) => x.fmt(f),
+        }
+    }
+}
+
+fn evaluate(binary: String, format: FloatFormat, raw: bool) {
+    let f = match FloatWrapper::parse(&binary, format) {
+        Ok(f) => f,
+        Err(err) => {
+            eprintln!("Error: {err}");
+            std::process::exit(1)
+        }
+    };
+
+    let out = if raw {
+        Value::Number(f.raw_value(), false)
+    } else {
+        f.evaluate()
+    };
+
+    println!("{f} => {out}");
 }
 
 fn plot(path: PathBuf) {
@@ -69,32 +146,10 @@ fn main() {
 
     match cli.cmd {
         Cmd::Plot { output_file } => plot(output_file),
-        Cmd::Evaluate { binary, raw } => {
-            if binary.to_lowercase() == "all" {
-                let all = SmallFloat::generate_all();
-                for f in all {
-                    let out = if raw {
-                        Value::Number(f.raw_value(), false)
-                    } else {
-                        f.evaluate()
-                    };
-
-                    println!("{f} => {out}");
-                }
-            } else {
-                let f = SmallFloat::parse(&binary).expect("No valid SmallFloat");
-                let out = if raw {
-                    Value::Number(f.raw_value(), false)
-                } else {
-                    f.evaluate()
-                };
-
-                println!("{f} => {out}");
-            }
-        }
-        Cmd::Test { binary } => {
-            let f = Ieee754::<5, 10>::parse(binary).expect("No valid Ieee754<5,10>");
-            println!("{f} => {}", f.evaluate());
-        }
+        Cmd::Evaluate {
+            binary,
+            raw,
+            format,
+        } => evaluate(binary, format, raw),
     }
 }
